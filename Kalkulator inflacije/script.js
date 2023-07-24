61a42e7ff8884f9d340dccca48f69a4050d56945
Mihael <simovmihael@gmail.com>
2023-07-24 17:12:49 +0200

    added Kalkulator Inflacije
    added my project which calculates inflation using inflation rate, amount of money and amount of years that are inputted by user.

---
+++ Kalkulator inflacije/script.js
@@ -0,0 +1,20 @@
+function inflationCalculator(){
+    let inflationRate = parseFloat(document.querySelector('#inflationRate').value)
+    let money = parseFloat(document.querySelector(`#money`).value)
+    let years = parseFloat(document.querySelector(`#years`).value)
+    let rezultat = money +(money * (inflationRate/100))
+    let rezultatPrikaz = document.querySelector(`#rezultat`)
+    
+    for (let i = 1; i < years; i++){
+        rezultat += rezultat * (inflationRate/100);
+    }
+
+    console.log(`
+    STOPA INFLACIJE: ${inflationRate}%
+    PARE: ${money}
+    GODINE: ${years}
+    REZULTAT: ${rezultat}
+    `)
+    rezultatPrikaz.innerText = `
+    ${money}€ ce za ${years} godina vrijediti ${rezultat.toFixed(2)}€.`
+}
