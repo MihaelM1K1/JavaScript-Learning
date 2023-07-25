adc42f5f4c8fc07558ecdac348e098ef05ab70e7
Mihael <simovmihael@gmail.com>
2023-07-25 23:34:59 +0200

    finished ToDoList project

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -2,16 +2,26 @@ function newElement(){
     let ul = document.querySelector('#list')
     let li = document.createElement('li')
     let textInput = document.querySelector('#textInput')
-    let linum = document.querySelectorAll("li").length;
+    let linum = (document.querySelectorAll("li").length)+1;
+    let closebutton=document.createElement('span')
     li.innerText= textInput.value
     li.classList='todo-li'
     li.setAttribute("id",`li${linum++}`)
     li.setAttribute("onclick",`check(id)`)
     ul.appendChild(li)
+    closebutton.innerText= `X`
+    closebutton.classList= 'close'
+    closebutton.setAttribute("id", `lispan${linum-1}`)
+    closebutton.setAttribute("onclick", "liDelete(id)")
+    li.appendChild(closebutton)
 
     textInput.value=''
 }
 function check(id){
     liItemCheck=document.querySelector(`#${id}`)
     liItemCheck.classList.toggle("checked")
+}
+function liDelete(id){
+    let childButton=document.querySelector(`#${id}`)
+    childButton.parentElement.remove()
 }
