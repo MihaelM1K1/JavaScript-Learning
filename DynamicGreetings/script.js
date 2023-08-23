f8b9944f464f12b2a3f3dd941b0f9a70e89d3531
Mihael <simovmihael@gmail.com>
2023-08-23 21:16:54 +0200

    changed variable name of DynamicGreetings
    changed variable name of DynamicGreetings so its easier to import into other projects

--- DynamicGreetings/script.js
+++ DynamicGreetings/script.js
@@ -1,17 +1,17 @@
 const now = new Date();
 const hours = now.getHours();
 
-textHTML = document.querySelector('#Greeting')
+GreetingEl = document.querySelector('#Greeting')
 
 if(21<=hours || 3>=hours){
-    textHTML.innerText = `Good night`
+    GreetingEl.innerText = `Good night`
 }
 else if (12>=hours){
-    textHTML.innerText = `Good morning`
+    GreetingEl.innerText = `Good morning`
 } 
 else if (17>=hours){
-    textHTML.innerText = `Good afternoon`
+    GreetingEl.innerText = `Good afternoon`
 }
 else if (20>=hours){
-    textHTML.innerText = `Good evening`
+    GreetingEl.innerText = `Good evening`
 }
